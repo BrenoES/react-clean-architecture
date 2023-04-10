37e5bed65c8a10faea7604995fbe195b32f36d1d
brenoes <breno.esilva@outlook.com>
2023-04-09 23:10:00 -0300

    refactor: move input to  a separate file

---
+++ src/presentation/components/input/input.tsx
@@ -0,0 +1,16 @@
+import styles from './input.module.scss'
+type Props = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>
+
+const Input: React.FC<Props> = (props: Props) => {
+  return (
+    <div className={styles.inputWrap}>
+      <input {...props} />
+      <span className={styles.status}></span>
+    </div>
+  )
+}
+
+export default Input
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import styles from './login-styles.module.scss'
 import Header from '@presentation/components/login-header/login-header'
 import Footer from '@presentation/components/footer/footer'
+import Input from '@presentation/components/input/input'
 
 const Login: React.FC = () => {
   return (
@@ -11,19 +12,8 @@ const Login: React.FC = () => {
       <Header />
       <form className={styles.form}>
         <h2>Login</h2>
-        <div className={styles.inputWrap}>
-          <input type="email" name="email" placeholder="Digite seu e-mail" />
-          <span className={styles.status}></span>
-        </div>
-        <div className={styles.inputWrap}>
-          <input
-            type="password"
-            name="password"
-            placeholder="Digite sua senha"
-          />
-          <span className={styles.status}></span>
-        </div>
-
+        <Input type="email" name="email" placeholder="Digite seu e-mail" />
+        <Input type="password" name="password" placeholder="Digite sua senha" />
         <button className={styles.submit} type="submit">
           Entrar
         </button>
